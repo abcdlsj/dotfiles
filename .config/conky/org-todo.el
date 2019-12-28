4a5a80285a5ed167eea0e01010bf9c5f6ce89ccf
abcdlsj <lisongjianshuai@gmail.com>
2019-12-28 22:45:16 +0800

    branch gentoo

--- .config/conky/org-todo.el
+++ .config/conky/org-todo.el
@@ -1,18 +1,19 @@
 (require 'org)
-(setq org-agenda-files (list "~/Dropbox/org/"))
+(setq org-directory nil)
+(setq org-capture-templates nil)
+(setq org-agenda-files (list "~/Arch/home/abcdlsj/Dropbox/org/"))
 (setq org-agenda-file-regexp "\\`[^.].*\\.org\\|.todo\\'")
 (setq org-capture-templates
       '(
-        ("t" "Task To Do!" entry
-         (file+headline "~/Dropbox/org/task.org" "GTD")
-         "* TODO %^{Task Name:}\n%u\n%a\n" :clock-in t :clock-resume t)
-        ("r" "Book Reading Task" entry
-         (file+headline "~/Dropbox/org/task.org" "Reading")
-         "* TODO %^{Book Name:}\n%u\n%a\n" :clock-in t :clock-resume t)
-        ("j" "Journal!!!" entry
-         (file+olp+datetree "~/Dropbox/org/journal.org")
-         "* %U - %^{heading} %^g\n %?\n" :tree-type week)
-        ("n" "Notes!!!" entry
-         (file+headline "~/Dropbox/org/notes.org" "NOTES")
-         "* %U - %^{heading} %^g\n %?\n")
-        ))
+	("t" "Task To Do!" entry
+	 (file+headline "~/Arch/home/abcdlsj/Dropbox/org/task.org" "GTD")
+	 "* TODO %^{Task Name:}\n%u\n%a\n" :clock-in t :clock-resume t)
+	("r" "Book Reading Task" entry
+	 (file+headline "~/Arch/home/abcdlsj/Dropbox/org/task.org" "Reading")
+	 "* TODO %^{Book Name:}\n%u\n%a\n" :clock-in t :clock-resume t)
+	("j" "Journal!!!" entry
+	 (file+olp+datetree "~/Arch/home/abcdlsj/Dropbox/org/journal.org")
+	 "* %U - %^{heading} %^g\n %?\n" :tree-type week)
+	("n" "Notes!!!" entry
+	 (file+headline "~/Arch/home/abcdlsj/Dropbox/org/notes.org" "NOTES")
+	 "* %U - %^{heading} %^g\n %?\n")))
